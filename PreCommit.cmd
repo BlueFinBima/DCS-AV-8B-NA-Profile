0eba422aaf1bb26aaf0be98a9d4dd7ad5257e733
BlueFinBima <BlueFinBima@users.noreply.github.com>
2021-11-27 10:59:56 +0000

    PreCommit script added
    PreCommit build script added, and minor changes made to the clock visibility in the profile

--- PreCommit.cmd
+++ PreCommit.cmd
@@ -11,7 +11,7 @@ rmdir .\temp /s
 @mkdir .\temp
 @mkdir .\temp\PreCommit
 @move ".\AV-8BNA.hpf" ".\temp\Precommit\AV-8BNA.hpf.candidate"
-powershell -command "Copy-Item -Path \".\AV-8B\Helios\Profiles\AV-8BNA.hpf\" -Destination (\".\temp\precommit\AV-8BNA \" + (get-Date -format \"yyyyMMddHHmm\") + \".hpf.bak\")"
+@powershell -command "Copy-Item -Path \".\AV-8B\Helios\Profiles\AV-8BNA.hpf\" -Destination (\".\temp\precommit\AV-8BNA \" + (get-Date -format \"yyyyMMddHHmm\") + \".hpf.bak\")"
 @echo A backup of original profile saved as ".\temp\Precommit\AV-8BNA yyyyMMddHHmm.hpf.bak"
 @type ".\temp\Precommit\AV-8BNA.hpf.candidate" | powershell -command "$input | ForEach-Object{$_ -replace '(<StaticValue>)[\d*]\.[\d*]\.[\d*](<\/StaticValue>)','$1profileversionnumber$2'}" > AV-8B\Helios\Profiles\AV-8BNA.hpf
 @Echo * * * .\AV-8B\Helios\Profiles\AV-8BNA.hpf has been created - Check and confirm that all is OK
